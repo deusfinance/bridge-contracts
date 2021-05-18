8276e13419bbdcf6ec0fa78acc79b15d9b0313d0
Sadegh Teymouri <sadeghte@gmail.com>
2021-05-19 02:09:17 +0430

    muon node signatures init

--- contracts/DeusBridge.sol
+++ contracts/DeusBridge.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./MuonV01.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 interface StandardToken {
     function balanceOf(address account) external view returns (uint256);
@@ -13,9 +15,28 @@ interface StandardToken {
 
 contract DeusBridge is Ownable{
     using SafeMath for uint256;
+    using ECDSA for bytes32;
 
     // we assign a unique ID to each chain
     uint256 public network;
+    mapping (uint256 => address) public sideContracts;
+    address public muonContract;
+
+    event Deposit(
+        uint256 txId,
+        uint256 tokenId,
+        uint256 amount,
+        uint256 indexed toChain,
+        address indexed user
+    );
+
+    event Claim(
+        address indexed user,
+        uint256 amount, 
+        uint256 indexed fromChain, 
+        uint256 tokenId, 
+        uint256 txId
+    );
 
     // tokenId => tokenContractAddress
     mapping(uint256 => address) public tokens;
@@ -35,28 +56,28 @@ contract DeusBridge is Ownable{
 
     mapping(uint256 => mapping(uint256 => bool)) public claimedTxs;
 
-    constructor(
-        uint256 _network
-    ){
+    constructor(uint256 _network, address _muon){
         network = _network;
+        muonContract = _muon;
     }
 
     function deposit(uint256 amount, uint256 toChain,
-        uint256 tokenId) public {
-        depositFor(msg.sender, amount, toChain, tokenId);
+        uint256 tokenId) public returns (uint256){
+        return depositFor(msg.sender, amount, toChain, tokenId);
     }
 
     function depositFor(address user,
         uint256 amount, uint256 toChain,
         uint256 tokenId
-    ) public {
+    ) public returns (uint256){
+        require(toChain != network, "!selfDeposit");
         require(tokens[tokenId] != address(0), "!tokenId");
 
         StandardToken token = StandardToken(tokens[tokenId]);
         token.transferFrom(address(msg.sender), address(this), amount);
 
         uint256 txId = ++lastTxId;
-        txs[lastTxId] = TX({
+        txs[txId] = TX({
             txId: txId,
             tokenId: tokenId,
             fromChain: network,
@@ -65,14 +86,27 @@ contract DeusBridge is Ownable{
             user: user
         });
         userTxs[user][toChain].push(txId);
+        emit Deposit(txId, tokenId, amount, toChain, user);
+
+        return txId;
     }
 
     //TODO: add Muon signature
     function claim(address user,
         uint256 amount, uint256 fromChain, uint256 toChain,
-        uint256 tokenId, uint256 txId) public{
+        uint256 tokenId, uint256 txId, bytes[] calldata sigs) public{
 
+        require(sideContracts[fromChain] != address(0), 'side contract not exist');
         require(toChain == network, "!network");
+        require(sigs.length > 1, "!sigs");
+
+        bytes32 hash = keccak256(abi.encodePacked(sideContracts[fromChain], user, amount, fromChain, toChain, tokenId, txId));
+        hash = hash.toEthSignedMessageHash();
+
+        MuonV01 muon = MuonV01(muonContract);
+        bool isVerified = muon.verify(hash, sigs);
+
+        require(isVerified, "sigs not verified");
 
         //TODO: shall we support more than one chain in one contract?
         require(!claimedTxs[fromChain][txId], "alreay claimed");
@@ -83,6 +117,7 @@ contract DeusBridge is Ownable{
         //TODO: any fees?
         token.transfer(user, amount);
         claimedTxs[fromChain][txId] = true;
+        emit Claim(user, amount, fromChain, tokenId, txId);
     }
 
     function pendingTxs(uint256 fromChain, uint256[] calldata ids) public view returns(
@@ -100,6 +135,22 @@ contract DeusBridge is Ownable{
         return userTxs[user][toChain];
     }
 
+    function getTx(uint256 _txId) public view returns(
+        uint256 txId,
+        uint256 tokenId,
+        uint256 amount,
+        uint256 fromChain,
+        uint256 toChain,
+        address user
+    ){
+        txId = txs[_txId].txId;
+        tokenId = txs[_txId].tokenId;
+        amount = txs[_txId].amount;
+        fromChain = txs[_txId].fromChain;
+        toChain = txs[_txId].toChain;
+        user = txs[_txId].user;
+    }
+
     function ownerAddToken(
         uint256 tokenId, address tokenContract
     ) public onlyOwner{
@@ -110,8 +161,13 @@ contract DeusBridge is Ownable{
         uint256 _network
     ) public onlyOwner{
         network = _network;
+        delete sideContracts[network];
     }
 
+    function ownerSetSideContract(uint256 _network, address _addr) public onlyOwner{
+        require (network != _network, '!current contract');
+        sideContracts[_network] = _addr;
+    }
 
     function emergencyWithdrawETH(uint256 amount, address addr) public onlyOwner{
         require(addr != address(0));
---
+++ contracts/MuonV01.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract MuonV01 is Ownable {
+    using ECDSA for bytes32;
+
+    mapping(address => bool) public signers;
+    
+    function verify(bytes32 hash, bytes[] calldata sigs) public view returns (bool) {
+        require(sigs.length > 0, "sigs length should be > 0");
+        uint i;
+        address signer;
+        for(i=0 ; i<sigs.length ; i++){
+            signer = hash.recover(sigs[i]);
+            // require(attualSigner == signer, "Signature not confirmed");
+            if(signers[signer] != true)
+                return false;
+        }
+        return true;
+    }
+
+    function ownerAddSigner(address _signer) public onlyOwner {
+        signers[_signer] = true;
+    }
+
+    function ownerRemoveSigner(address _signer) public onlyOwner {
+        delete signers[_signer];
+    }
+}
